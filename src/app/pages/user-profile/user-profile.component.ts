d8a87d47e7ca4e428eb4c9a4365a38a6f83e5cbb
DylanMerlateau <dylan.merlateau@gmail.com>
2022-09-07 17:01:53 +0200

    Changement user-profile--->Gestion comptes

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Compte } from 'src/app/model/compte';
+import { CompteService } from 'src/app/services/compte.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  comptes!: any[];
+  compte : Compte = new Compte();
+  constructor(private compteService:CompteService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.findAllCompte();
   }
-
+  findAllCompte(){
+    this.compteService.findAll().subscribe((data: any[]) => {this.comptes = data;});
+  }
+  save(){
+    this.compteService.save(this.compte).subscribe(
+      ()=>{
+        this.findAllCompte(); //update list
+        this.compte = new Compte(); //vider formulaire
+      }
+    )
+  }
+  delete(id:number){
+    this.compteService.delete(id).subscribe(()=>{this.findAllCompte()});
+  }
 }
