0da60d990312cbfe49ebcfdcfd9e82e8c366a896
Eloïc F <eloic.ferdinand@gmail.com>
2022-09-07 13:19:14 +0200

    classe personne

--- src/app/model/personne.ts
+++ src/app/model/personne.ts
@@ -1,2 +1,20 @@
 export class Personne {
+  id!: number;
+  nom!: string;
+  prenom!: string;
+  dateNaissance!: Date;
+  email!: string;
+  numeroTel!: number;
+  adressePersonne!: Adresse;
+  image!: File;
+  username!: string;
+  password!: string;
+  classe!: Classe;
+  universite!: Universite;
+  compte!: Compte;
+  examens!: Examen[];
+  evaluations!: Evaluation[];
+  courses!: Cours[];
+  questions!: Question[];
+  roles!: Role[];
 }
