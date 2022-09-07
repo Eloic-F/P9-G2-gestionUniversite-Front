fe9b221252b3e4590ab3581011ad24d1e1ecffa1
MenguGay <enguerrangay@gmail.com>
2022-09-07 13:59:08 +0200

    Correction mineur

--- src/app/model/section.ts
+++ src/app/model/section.ts
@@ -1,7 +1,9 @@
 import { Formation } from "./formation";
+import { Universite } from "./universite";
 
 export class Section {
     idSection!:number;
     nomSection!:string;
+    universite!:Universite;
     formations!:Formation[];
 }
--- src/app/model/universite.ts
+++ src/app/model/universite.ts
@@ -2,11 +2,13 @@ import { CentreDeRecherche } from "./centre-de-recherche";
 import { Personne } from "./personne";
 import { Section } from "./section";
 import { Adresse } from "./adresse";
+import { Academie } from "./academie";
 
 export class Universite {
     idUniversite!:number;
     nomUniversite!:string;
-    adresseUniversite!:adresse;
+    adresseUniversite!:Adresse;
+    academie!:Academie;
     centreDeRecherches!:CentreDeRecherche[];
     personnes!:Personne[];
     sections!:Section[];
