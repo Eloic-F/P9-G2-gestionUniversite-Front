97074676f67626cc98ab57baa96d89451e6cce7b
Eloïc F <eloic.ferdinand@gmail.com>
2022-09-07 13:35:03 +0200

    service evaluation

---
+++ src/app/services/evaluation.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Evaluation } from '../model/evaluation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvaluationService {
+  private baseUrl=environment.apiBaseUrl+"evaluations"
+  constructor(private httpClient:HttpClient) { }
+
+  public findAll():Observable<any>{
+    return this.httpClient.get<any>(`${this.baseUrl}`);
+  }
+
+  public addEvaluation(evaluation:Evaluation):Observable<any>{
+    return this.httpClient.post<any>(`${this.baseUrl}`, evaluation);
+  }
+
+  public deleteEvaluation(idEvaluation:number):Observable<void>{
+    return this.httpClient.delete<any>(`${this.baseUrl}/${idEvaluation}`);
+  }
+
+  /*public updateEvaluation(evaluation:Evaluation):Observable<any>{
+    return this.httpClient.put<any>(`${this.baseUrl}`, evaluation);
+  }*/
+}
